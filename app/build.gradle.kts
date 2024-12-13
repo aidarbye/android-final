2e97f409260c39b79f5874dd4b9bb65bc3971826
Aidar <aidar.nurkin@icloud.com>
2024-12-14 01:14:02 +0500

    login func

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.android.material:material:1.8.0")
 }
--- app/src/main/java/com/example/androidfinalxml/activity/AuthActivity.kt
+++ app/src/main/java/com/example/androidfinalxml/activity/AuthActivity.kt
@@ -1,7 +1,10 @@
 package com.example.androidfinalxml.activity
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -10,44 +13,101 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidfinalxml.R
 import com.example.androidfinalxml.viewmodel.AuthViewModel
+import com.google.android.material.internal.ViewUtils.hideKeyboard
+import com.google.android.play.integrity.internal.s
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var authViewModel: AuthViewModel
 
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        // Инициализация ViewModel
+        val loginButton: Button = findViewById(R.id.loginButton)
+        emailEditText = this.findViewById(R.id.emailEditText)
+        passwordEditText = this.findViewById(R.id.passwordEditText)
+
         authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
 
-        // Наблюдение за состоянием авторизации
         authViewModel.isLoggedIn.observe(this, Observer { isLoggedIn ->
             if (isLoggedIn) {
-                // Если пользователь авторизован, переходим в HomeActivity
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
                 finish()
             }
         })
 
-        // Логика авторизации
-        val loginButton: Button = findViewById(R.id.loginButton)
-        loginButton.setOnClickListener {
-            val email = findViewById<EditText>(R.id.emailEditText).text.toString()
-            val password = findViewById<EditText>(R.id.passwordEditText).text.toString()
-
-            authViewModel.firebaseAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Авторизация прошла успешно, обновляем состояние
-                        authViewModel.checkAuthStatus()
-                    } else {
-                        // Ошибка авторизации
-                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                    }
-                }
+        emailEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == android.view.inputmethod.EditorInfo.IME_ACTION_DONE) {
+                passwordEditText.requestFocus()
+                true
+            } else {
+                false
+            }
         }
+
+        passwordEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == android.view.inputmethod.EditorInfo.IME_ACTION_DONE) {
+                passwordEditText.clearFocus()
+                hideKeyboard(v)
+                true
+            } else {
+                false
+            }
+        }
+
+        loginButton.setOnClickListener {
+            if (emailEditText.text.isEmpty() || passwordEditText.text.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Поля не могут быть пустыми",
+                    Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            login()
+        }
+    }
+
+    private fun hideKeyboard(view: android.view.View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun login() {
+
+        val email: String = emailEditText.text.toString()
+        val password: String = passwordEditText.text.toString()
+
+        authViewModel.firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    authViewModel.checkAuthStatus()
+                } else {
+                    when (task.exception) {
+                        is FirebaseAuthInvalidCredentialsException -> {
+                            Toast.makeText(this, "Неверные учетные данные. Пожалуйста, проверьте ваш email и пароль.", Toast.LENGTH_SHORT).show()
+                        }
+                        is FirebaseAuthInvalidUserException -> {
+                            Toast.makeText(this, "Аккаунт с таким email не найден.", Toast.LENGTH_SHORT).show()
+                        }
+                        is FirebaseAuthWeakPasswordException -> {
+                            Toast.makeText(this, "Пароль слишком слабый. Пожалуйста, выберите более сложный пароль.", Toast.LENGTH_SHORT).show()
+                        }
+                        else -> {
+                            Toast.makeText(this, "Ошибка авторизации. ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+            }
+
     }
 }
